3e41b71fc0e7e883ad56e5ecbfe9ab56d687db0b
Nikolay Antonovich <TesaloNick@gmail.com>
2021-03-02 21:43:34 +0300

    Finish

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const sliderContainer = document.getElementById('image-container')
+const mainContainer = document.querySelector('.image-container')
 
 const slides = new Array(10)
 slides.fill('')                     // наполнить массив хоть чем-то, чтобы перебрать массив в последующем
@@ -9,28 +10,36 @@ slides.forEach((slide, index) => {  // slides.map((slide, index)
     slide.src = `images/${index+1}.jpg`
     sliderContainer.appendChild(slide)
 })
+const arrowLeft = document.createElement('span')
+const arrowRight = document.createElement('span')
 
-const drawArrows = () => {
-    const arrowLeft = document.createElement('span')
+const drawArrows = () => {          // рисуем стрелив влево и вправо
     arrowLeft.innerHTML = '<'
-    sliderContainer.prepend(arrowLeft)
-    const arrowRight = document.createElement('span')
+    mainContainer.prepend(arrowLeft)
     arrowRight.innerHTML = '>'
-    sliderContainer.appendChild(arrowRight)
+    mainContainer.appendChild(arrowRight)
     arrowRight.addEventListener('click', slideRight)
     arrowLeft.addEventListener('click', slideLeft)
 }
 
 drawArrows()
-let STEP = 0;
 
+let STEP = 0;
 function slideRight() {
-    console.log(sliderContainer.querySelector('img'));
-    ++STEP
-    sliderContainer.style.marginLeft = `-${STEP*300}px`
+    if (STEP === slides.length-4) {              // блокировать движение вправо
+        arrowLeft.style.opacity = 0.5;
+        arrowLeft.style.pointerEvent = 'none';
+    } else {
+        ++STEP
+        sliderContainer.style.transform = `translate3d(-${STEP*300}px, 0, 0)`    //     sliderContainer.style.margin = `-${STEP*300}px`
+    }
 }
 function slideLeft() {
-    console.log(sliderContainer.querySelector('img'));
-    --STEP
-    sliderContainer.style.marginLeft = `${-STEP*300}px`
+    if (STEP === 0) {
+        arrowLeft.style.opacity = 0.5;
+        arrowLeft.style.pointerEvent = 'none';
+    } else {
+        --STEP
+        sliderContainer.style.transform = `translate3d(-${STEP*300}px, 0, 0)`
+    }
 }
